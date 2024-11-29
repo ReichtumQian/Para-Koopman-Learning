6cc0dad78088eeb172e89f1815ddca25dc5fefbb
reichtum <qyx18267223529@outlook.com>
2024-11-29 16:21:18 +0800

    update fhn example.

--- PKoopmanDL/Net.py
+++ PKoopmanDL/Net.py
@@ -7,7 +7,8 @@ class FullConnBaseNet(torch.nn.Module):
                input_dim=1,
                output_dim=1,
                layer_size=[64, 64],
-               activation='tanh'):
+               activation='tanh',
+               normalize_input=True):
     """ Initialize the FullConnResNet instance.
 
     Args:
@@ -17,14 +18,13 @@ class FullConnBaseNet(torch.nn.Module):
         activation (str, optional): The activation function. Defaults to 'tanh'.
     """
     super().__init__()
+    self._normalize_input = normalize_input
     # input layer
     self._input_layer = torch.nn.Linear(input_dim, layer_size[0], bias=False)
     # hidden layers
     self._hidden_layers = torch.nn.ModuleList()
-    self._batch_norms = torch.nn.ModuleList()
     for in_features, out_features in zip(layer_size[:-1], layer_size[1:]):
       self._hidden_layers.append(torch.nn.Linear(in_features, out_features))
-      self._batch_norms.append(torch.nn.BatchNorm1d(out_features))
     # output layer
     self._output_layer = torch.nn.Linear(layer_size[-1], output_dim)
     self._activation_type = activation
@@ -36,6 +36,13 @@ class FullConnBaseNet(torch.nn.Module):
       return torch.relu(x)
     else:
       raise NotImplementedError
+
+  def _normalize(self, inputs):
+    min_val = inputs.min(dim=0, keepdim=True)[0]
+    max_val = inputs.max(dim=0, keepdim=True)[0]
+    normalized_inputs = (inputs - min_val) / (
+        max_val - min_val + 1e-7)  # Add epsilon to prevent division by zero
+    return normalized_inputs
 
   def forward(self, inputs):
     """Apply the network to the input `inputs`
@@ -52,11 +59,12 @@ class FullConnBaseNet(torch.nn.Module):
 class FullConnResNet(FullConnBaseNet):
 
   def forward(self, inputs):
+    if self._normalize_input:
+      inputs = self._normalize(inputs)
     hidden_u = self._input_layer(inputs)
-    for layer, batch_norm in zip(self._hidden_layers, self._batch_norms):
+    for layer in self._hidden_layers:
       residual = hidden_u
       hidden_u = layer(hidden_u)
-      hidden_u = batch_norm(hidden_u)
       hidden_u = self._apply_activation(hidden_u)
       hidden_u = hidden_u + residual
     return self._output_layer(hidden_u)
@@ -65,9 +73,10 @@ class FullConnResNet(FullConnBaseNet):
 class FullConnNet(FullConnBaseNet):
 
   def forward(self, inputs):
+    if self._normalize_input:
+      inputs = self._normalize(inputs)
     hidden_u = self._input_layer(inputs)
-    for layer, batch_norm in zip(self._hidden_layers, self._batch_norms):
+    for layer in self._hidden_layers:
       hidden_u = layer(hidden_u)
-      hidden_u = batch_norm(hidden_u)
       hidden_u = self._apply_activation(hidden_u)
     return self._output_layer(hidden_u)
